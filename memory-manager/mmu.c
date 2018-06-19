f6dc88eb28731bc71b196d5d3242aeb1b3bb59f5
Murygin Maksim <mail@muryginm.com>
2018-06-20 01:09:55 +0700

    update

--- memory-manager/mmu.c
+++ memory-manager/mmu.c
@@ -1,8 +1,10 @@
 #include "include/mmu.h"
 
+static uint64_t _root_table_addr;
+
 void set_root_table_addr(uint64_t root_table_addr)
 {
-
+    _root_table_addr = root_table_addr;
 }
 
 bool get_physical_addr(uint64_t logical_addr, uint64_t *physical_addr)
