fbfe2a3faaf9a8a4c904791c842a2759fb58b070
Murygin Maksim <mail@muryginm.com>
2018-07-03 13:31:21 +0700

    update

--- allocator/allocator.c
+++ allocator/allocator.c
@@ -2,28 +2,36 @@
 #include "allocator.h"
 
 typedef struct free_unit {
-    FreeUnit * prev;
-    FreeUnit * next;
-    Header header;
+    struct free_unit * prev;
+    struct free_unit * next;
+    size_t size;
 } FreeUnit;
 
-typedef struct header
-{
+typedef struct end_header {
     size_t size;
     bool is_free;
-} Header;
+} EndHeader;
+
+typedef struct busy_unit {
+    size_t size;
+} BusyUnit;
 
 static FreeUnit *free_units_head;
 
-size_t get_extra_size();
-void init_free_unit(FreeUnit * free_unit, size_t size);
+size_t free_unit_extras();
+size_t busy_unit_extras();
+
+void init_free_unit(FreeUnit *free_unit, size_t available_size);
+void init_busy_unit(BusyUnit *busy_unit, size_t size);
 FreeUnit * get_free_unit(FreeUnit * header, size_t size);
-Header * split_unit(FreeUnit * free_unit, size_t new_unit_size);
+BusyUnit * split_free_unit(FreeUnit * free_unit, size_t busy_unit_size);
 
 void mysetup(void *buf, size_t size)
 {
     free_units_head = (FreeUnit*) buf;
-    init_free_unit(free_units_head, size);
+
+    size_t real_size = size - free_unit_extras();
+    init_free_unit(free_units_head, real_size);
 }
 
 void *myalloc(size_t size)
@@ -35,14 +43,15 @@ void *myalloc(size_t size)
         return NULL;
     }
 
-    if (free_unit->header.size = size)
+    if (free_unit->size = size)
     {
         // TODO(MM): handle if free_unit == free_units_head
 
     }
 
-    Header * new_unit = split_unit(free_unit, size);
-    return (char*) new_unit + sizeof(Header);
+    BusyUnit * busy_unit = split_free_unit(free_unit, size);
+
+    return (char*) busy_unit + sizeof(BusyUnit);
 }
 
 void myfree(void *p)
@@ -50,39 +59,40 @@ void myfree(void *p)
 
 }
 
-/*
-| *prev | *next | size | is_free | ------------------------- | size | is_free |
-*/
-void init_free_unit(FreeUnit * free_unit, size_t size)
+void create_end_header(void *buffer_end, size_t size, bool is_free)
 {
-    size_t real_size = size - get_extra_size();
-
-    free_unit->prev = NULL;
-    free_unit->next = NULL;
-    free_unit->header.is_free = true;
-    free_unit->header.size = real_size;
-
-    Header *header = (Header *)((char *)free_unit + size - sizeof(FreeUnit));
-    header->is_free = true;
-    header->size = real_size;
+    EndHeader *end_header = (EndHeader*)((char *)buffer_end - sizeof(EndHeader));
+    end_header->is_free = is_free;
+    end_header->size = size;
 }
 
 /*
-| size | is_free | ------------------------- | size | is_free |
+| *prev | *next | size  | ------------------------- | size | is_free |
 */
-void init_busy_unit(Header * busy_unit, size_t size)
+void init_free_unit(FreeUnit *free_unit, size_t size)
 {
-    busy_unit->is_free = false;
+    free_unit->prev = NULL;
+    free_unit->next = NULL;
+    free_unit->size = size;
+
+    char *buffer_end = (char *)free_unit + size + free_unit_extras();
+    create_end_header(buffer_end, size, true);
+}
+
+/*
+| size | ------------------------- | size | is_free |
+*/
+void init_busy_unit(BusyUnit * busy_unit, size_t size)
+{
     busy_unit->size = size;
 
-    Header *end_header = (Header*)((char*)busy_unit + size + sizeof(Header));
-    end_header->is_free = false;
-    end_header->size = size;
+    char *buffer_end = (char *)busy_unit + size + busy_unit_extras();
+    create_end_header(buffer_end, size, true);
 }
 
 FreeUnit * get_free_unit(FreeUnit * free_unit, size_t size)
 {
-    if (free_unit->header.size >= size)
+    if (free_unit->size>= size)
     {
         return free_unit;
     }
@@ -95,20 +105,25 @@ FreeUnit * get_free_unit(FreeUnit * free_unit, size_t size)
     return NULL;
 }
 
-size_t get_extra_size()
+size_t free_unit_extras()
 {
-    return sizeof(FreeUnit) + sizeof(Header);
+    return sizeof(FreeUnit) + sizeof(EndHeader);
 }
 
-Header *split_unit(FreeUnit *free_unit, size_t new_unit_size)
+size_t busy_unit_extras()
 {
-    size_t taken_size = new_unit_size + get_extra_size();
-    size_t remained_size = free_unit->header.size - taken_size;
+    return sizeof(BusyUnit) + sizeof(EndHeader);
+}
+
+BusyUnit *split_free_unit(FreeUnit *free_unit, size_t busy_unit_size)
+{
+    size_t taken_size = busy_unit_size + busy_unit_extras();
+    size_t remained_size = free_unit->size - taken_size;
 
     init_free_unit(free_unit, remained_size);
 
-    Header *busy_unit = (Header*)((char *)free_unit + free_unit->header.size - taken_size);
-    init_busy_unit(busy_unit, new_unit_size);
+    BusyUnit *busy_unit = (BusyUnit*)((char*) free_unit + free_unit->size + free_unit_extras());
+    init_busy_unit(busy_unit, busy_unit_size);
 
     return busy_unit;
 }
