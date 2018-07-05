4185bd2ebc419c586880c275dd09513ca03fda60
Murygin Maksim <mail@muryginm.com>
2018-07-05 23:02:30 +0700

    refactor freeing

--- allocator/allocator.c
+++ allocator/allocator.c
@@ -15,8 +15,6 @@ typedef struct free_unit
 } FreeUnit;
 
 static FreeUnit *free_units_head;
-static void * left_ptr;
-static void * right_ptr;
 static size_t min_size;
 
 size_t busy_unit_extras();
@@ -24,13 +22,13 @@ size_t free_unit_extras();
 
 void init_free_unit(FreeUnit *free_unit, size_t size);
 void init_busy_unit(Header *busy_unit, size_t size);
-FreeUnit * get_free_unit(FreeUnit * header, size_t size);
+void merge_right_unit(Header *busy_unit);
+Header * merge_left_unit(Header *busy_unit);
+FreeUnit *get_free_unit(FreeUnit *header, size_t size);
 Header * split_free_unit(FreeUnit * free_unit, size_t busy_unit_size);
 
 void mysetup(void *buf, size_t size)
 {
-    left_ptr = buf;
-    right_ptr = (char*) buf + size;
     free_units_head = (FreeUnit*) buf;
     min_size = free_unit_extras() - busy_unit_extras();
 
@@ -59,13 +57,13 @@ void *myalloc(size_t size)
 
     Header *busy_unit = NULL;
 
-    if (free_unit->header.size <= size + free_unit_extras() - busy_unit_extras())
+    if (free_unit->header.size <= size + min_size)
     {
         if (free_unit->prev)
         {
             free_unit->prev->next = free_unit->next;
         }
-        else if (free_unit == free_units_head)
+        else
         {
             free_units_head = free_unit->next;
         }
@@ -89,52 +87,8 @@ void myfree(void *p)
     }
 
     Header *busy_unit = (Header*)((char*)p - sizeof(Header));
-    if (p > left_ptr)
-    {
-        Header *left_header = (Header*)((char*)busy_unit - sizeof(Header));
-
-        if (left_header->is_free)
-        {
-            FreeUnit* free_unit = (FreeUnit*)((char*)left_header - left_header->size - sizeof(Header));
-            if (free_unit->prev)
-            {
-                free_unit->prev->next = free_unit->next;
-            }
-            else
-            {
-                free_units_head = free_unit->next;
-            }
-
-            size_t merged_size = free_unit->header.size + busy_unit->size + busy_unit_extras();
-
-            busy_unit = (Header*)free_unit;
-            init_busy_unit(busy_unit, merged_size);
-        }
-    }
-
-    if (p < right_ptr)
-    {
-        Header *right_header = (Header*)((char *)busy_unit + busy_unit->size +
-            busy_unit_extras());
-
-        if ((void*)right_header < right_ptr && right_header->is_free)
-        {
-            FreeUnit *free_unit = (FreeUnit *)right_header;
-
-            if (free_unit->prev)
-            {
-                free_unit->prev->next = free_unit->next;
-            }
-            else
-            {
-                free_units_head = free_unit->next;
-            }
-
-            size_t merged_size = free_unit->header.size + busy_unit->size + busy_unit_extras();
-
-            init_busy_unit(busy_unit, merged_size);
-        }
-    }
+    busy_unit = merge_left_unit(busy_unit);
+    merge_right_unit(busy_unit);
 
     FreeUnit *new_free_unit = (FreeUnit*)busy_unit;
     init_free_unit(new_free_unit, busy_unit->size);
@@ -145,6 +99,55 @@ void myfree(void *p)
         free_units_head->prev = new_free_unit;
     }
     free_units_head = new_free_unit;
+}
+
+Header * merge_left_unit(Header *busy_unit)
+{
+    Header *left_header = (Header *)((char *)busy_unit - sizeof(Header));
+    if (left_header->is_free)
+    {
+        FreeUnit *free_unit = (FreeUnit *)((char *)left_header - left_header->size - sizeof(Header));
+        if (free_unit->prev)
+        {
+            free_unit->prev->next = free_unit->next;
+        }
+        else
+        {
+            free_units_head = free_unit->next;
+        }
+
+        size_t merged_size = free_unit->header.size + busy_unit->size + busy_unit_extras();
+
+        Header * merged_unit = (Header *)free_unit;
+        init_busy_unit(merged_unit, merged_size);
+
+        return merged_unit;
+    }
+
+    return busy_unit;
+}
+
+void merge_right_unit(Header * busy_unit)
+{
+    Header *right_header = (Header *)((char *)busy_unit + busy_unit->size + busy_unit_extras());
+
+    if (right_header->is_free)
+    {
+        FreeUnit *free_unit = (FreeUnit *)right_header;
+
+        if (free_unit->prev)
+        {
+            free_unit->prev->next = free_unit->next;
+        }
+        else
+        {
+            free_units_head = free_unit->next;
+        }
+
+        size_t merged_size = free_unit->header.size + busy_unit->size + busy_unit_extras();
+
+        init_busy_unit(busy_unit, merged_size);
+    }
 }
 
 void create_end_header(const Header *start_header)
