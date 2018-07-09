c6eeb8ed367e8dad7d9b0fef3ff7705218a32612
Murygin Maksim <mail@muryginm.com>
2018-07-09 17:37:34 +0700

    finished planner

--- simple-planner/main.c
+++ simple-planner/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void quick_sort(unsigned *arr, unsigned *indexes, unsigned len);
+void quick_sort(unsigned *from, unsigned *to, unsigned *ifrom, unsigned *ito);
 void read_array_from_stdin(unsigned *arr, unsigned size);
 void init_indexes_array(unsigned *arr, unsigned size);
 void print_array(const unsigned *arr, unsigned len);
@@ -14,10 +14,10 @@ int main()
     unsigned *arr = calloc(length, sizeof(unsigned));
     read_array_from_stdin(arr, length);
 
-    unsigned * indexes_array = calloc(length, sizeof(unsigned));
+    unsigned *indexes_array = calloc(length, sizeof(unsigned));
     init_indexes_array(indexes_array, length);
 
-    quick_sort(arr, indexes_array, length);
+    quick_sort(arr, arr + length, indexes_array, indexes_array + length);
     print_array(indexes_array, length);
 
     free(arr);
@@ -26,22 +26,18 @@ int main()
     return 0;
 }
 
-void swap(unsigned *arr, unsigned *indexes, unsigned from, unsigned to)
+void swap(unsigned *arr, unsigned from, unsigned to)
 {
     unsigned arr_tmp = arr[from];
     arr[from] = arr[to];
     arr[to] = arr_tmp;
-
-    arr_tmp = indexes[from];
-    indexes[from] = indexes[to];
-    indexes[to] = arr_tmp;
 }
 
 void print_array(const unsigned *arr, unsigned len)
 {
     for (unsigned i = 0; i < len; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%u ", arr[i]);
     }
 
     putchar('\n');
@@ -63,7 +59,26 @@ void init_indexes_array(unsigned *arr, unsigned size)
     }
 }
 
-void quick_sort(unsigned *arr, unsigned *indexes, unsigned len)
+void quick_sort(unsigned *from, unsigned *to, unsigned *ifrom, unsigned *ito)
 {
-    return;
+    if (from >= to)
+    {
+        return;
+    }
+
+    int len = to - from;
+    int pivot = len - 1;
+
+    for (int i = len - 1; i > 0; --i)
+    {
+        if (from[i - 1] > from[pivot])
+        {
+            swap(from, i - 1, pivot);
+            swap(ifrom, i - 1, pivot);
+            pivot = i - 1;
+        }
+    }
+
+    quick_sort(from, from + pivot, ifrom, ifrom + pivot);
+    quick_sort(from + pivot + 1, to, ifrom + pivot + 1, ito);
 }
